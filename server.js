6a88d5748560c584531dbc2712db19af83a919a3
jualon92 <londrajuan@gmail.com>
2022-04-12 00:01:02 -0300

    listaDb

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ import { dirname } from 'path';
 import fetch from "node-fetch"
 
 
+const PORT = config.PORT
 
 mercadopago.configure({
     access_token: "APP_USR-415255069673949-013103-53933e062c46695b33189542f57470bc-92731233",
@@ -38,40 +39,38 @@ const fetchItems = async () => { //preguntar a mongo los pares nombre precio par
     listaDB = await fetch(`http://localhost:9000/items`).then(r => r.json())
 }
 
- 
+
 
 app.post("/create_preference", async (req, res) => {
-    
+
     listaDB = await fetchItems()
-     
- 
-        if (true) {
-            console.log(arrLocales)
-            let preference = {
-                items: arrLocales,
-                back_urls: {
-                    "success": "http://localhost:9000/feedback",
-                    "failure": "http://localhost:9000/feedback",
-                    "pending": "http://localhost:9000/feedback"
-                },
-                auto_return: "approved",
-            };
 
-            mercadopago.preferences.create(preference)
-                .then(function (response) {
-                    res.json({
-                        id: response.body.id
-                    });
-                }).catch(function (error) {
-                    console.log(error);
+
+    if (true) {
+        console.log(arrLocales)
+        let preference = {
+            items: arrLocales,
+            back_urls: {
+                "success": "http://localhost:9000/feedback",
+                "failure": "http://localhost:9000/feedback",
+                "pending": "http://localhost:9000/feedback"
+            },
+            auto_return: "approved",
+        };
+
+        mercadopago.preferences.create(preference)
+            .then(function (response) {
+                res.json({
+                    id: response.body.id
                 });
+            }).catch(function (error) {
+                console.log(error);
+            });
 
-        } else {
-            console.log("existe un item que no concuerda con la DB")
-        }
-    } catch {
-        console.log(error)
+    } else {
+        console.log("existe un item que no concuerda con la DB")
     }
+
 });
 
 app.get('/feedback', function (req, res) {
