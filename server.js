5272cb982a3aaffc986b92470d20758c3860e701
jualon92 <londrajuan@gmail.com>
2022-04-11 23:45:27 -0300

    prueba fetch

--- server.js
+++ server.js
@@ -34,19 +34,75 @@ app.use(express.json())
 app.use("/items", routerItem)
 
 
+//let listaDB  //deberia ser por import
+const fetchItems = async () => { //preguntar a mongo los pares nombre precio para 
+    listaDB = await fetch('http://localhost:9000/items').then(r => r.json())
 
-app.post("/create_preference",   (req, res) => {
+}
 
- 
+fetchItems()
+
+
+app.post("/create_preference", async (req, res) => {
+    /*
+
+    //  let itemInput = {name: req.body.description, price : req.body.price}
+    //  let listaItems = listaDB[0]
+    //  let itemBack = {name: listaItems.name, price: listaItems.price}
+    //  console.log(listaItems)
+    //   console.log(itemInput,itemBack)
+    //   console.log(JSON.stringify(itemInput) == JSON.stringify(itemBack))
+
+
+    //verificacion integridad
+    let arrReq = Array.from(req.body)
+
+    let listaMongo = listaDB
+
+
+    let arrLocales = []
+    let arrDB = []
+    for (let i = 0; i < arrReq.length; i++) { // setea preferencia de cada item
+        let producto = {
+            title: req.body[i].description,
+            unit_price: Number(req.body[i].price),
+            quantity: Number(req.body[i].quantity),
+        }
+        arrLocales.push(producto)
+    }
+
+
+    let arrLocalComparable = arrLocales.map(prod => JSON.stringify(({ title: prod.title, unit_price: prod.unit_price })))
+    let arrDBComparable = listaDB.map(prod => JSON.stringify(({ title: prod.name, unit_price: prod.price })))
+
+
+    //verificacion
+
+
+
+    console.log("lista local: ", arrLocalComparable)
+    console.log("lista db ", arrDBComparable)
+
+    let listaCoincidencias = []
+
+    function verificar() {
+
+        arrLocalComparable.forEach(localSTR => { //por cada item en la lista
+            let estaEnDB = arrDBComparable.includes(localSTR)
+            listaCoincidencias.push(estaEnDB)
+        });
+    }
+
+
+    const coincidenTodasEnDB = () => listaCoincidencias.every(ele => ele == true)
+    
+
+    verificar()
+    console.log(listaCoincidencias) */
+    if (true) {
         // if (JSON.stringify(listaItems) == JSON.stringify(arrAtributos)) {  //verificar
         let preference = {
-            items:  [
-                {
-                    title: req.body.description,
-                    unit_price: Number(req.body.price),
-                    quantity: Number(req.body.quantity),
-                }
-            ],
+            items: arrLocales,
             back_urls: {
                 "success": "http://localhost:8080/feedback",
                 "failure": "http://localhost:8080/feedback",
@@ -67,7 +123,9 @@ app.post("/create_preference",   (req, res) => {
              console.warn("precio con ese nombre no existe en DB")
          */
         //   }
-     
+    } else {
+        console.log("existe un item que no concuerda con la DB")
+    }
 });
 
 
